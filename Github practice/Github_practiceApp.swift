fc63c71311c72c8fc7ec4f74d2203eb7c25eae99
Mahati Madugula <mahati.madugula@icloud.com>
2023-06-12 10:25:18 -0500

    Initial Commit

---
+++ Github practice/Github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_practiceApp.swift
+//  Github practice
+//
+//  Created by Mahati Madugula on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
